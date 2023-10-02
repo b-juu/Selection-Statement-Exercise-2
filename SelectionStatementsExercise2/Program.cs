c1d132e5f1b0e5d7311320e14d0bea40a5125443
b-juu <erichassett4@gmail.com>
2023-10-02 12:48:21 -0400

    ex2 initial commit

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject");
+            var answer = Console.ReadLine();
+            switch (answer)
+            {
+                case "history":
+                    Console.WriteLine("history is interesting");
+                    break;
+                case "chemistry":
+                    Console.WriteLine("i love chemistry");
+                    break;
+                case "english":
+                    Console.WriteLine("i rarely use propper grammar");
+                    break;
+                case "physics":
+                    Console.WriteLine("very cool");
+                    break;
+                case "biology":
+                    Console.WriteLine("nice");
+                    break;
+                default:
+                    Console.WriteLine("i dont know that one");
+                    break;
+            }
         }
     }
 }
